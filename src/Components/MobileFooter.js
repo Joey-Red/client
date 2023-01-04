1e5201dbf24fb528b4d4d823816119e7c7908dc0
Joey <xjoey96@gmail.com>
2023-01-04 15:52:58 -0600

    styling changes

--- src/Components/MobileFooter.js
+++ src/Components/MobileFooter.js
@@ -7,31 +7,61 @@ import {
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 
-function MobileFooter() {
+function MobileFooter(props) {
+  let { loggedIn, setSignUpModalState, setLogInModalState } = props;
+  let handleSignUpState = () => {
+    setSignUpModalState(true);
+    setLogInModalState(false);
+  };
+  let handleLoginState = () => {
+    setLogInModalState(true);
+    setSignUpModalState(false);
+  };
   return (
     <div className="bg-white fixed bottom-0 right-0 left-0 border-light-gray border-t h-[52px]">
-      <div className="flex flex-row p-2">
-        <button className="flex m-auto">
-          <FontAwesomeIcon
-            icon={faHouse}
-            size="xl"
-            className="flex m-auto text-red-500"
-          />
-        </button>
-        <button className="flex m-auto">
-          <FontAwesomeIcon icon={faSearch} size="xl" className="flex m-auto" />
-        </button>
-        <button className="flex m-auto">
-          <FontAwesomeIcon icon={faBell} size="xl" className="flex m-auto" />
-        </button>
-        <button className="flex m-auto">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            size="xl"
-            className="flex m-auto"
-          />
-        </button>
-      </div>
+      {loggedIn ? (
+        <div className="flex flex-row p-2 h-full">
+          <button className="flex m-auto">
+            <FontAwesomeIcon
+              icon={faHouse}
+              size="xl"
+              className="flex m-auto text-red-500"
+            />
+          </button>
+          <button className="flex m-auto">
+            <FontAwesomeIcon
+              icon={faSearch}
+              size="xl"
+              className="flex m-auto"
+            />
+          </button>
+          <button className="flex m-auto">
+            <FontAwesomeIcon icon={faBell} size="xl" className="flex m-auto" />
+          </button>
+          <button className="flex m-auto">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              size="xl"
+              className="flex m-auto"
+            />
+          </button>
+        </div>
+      ) : (
+        <div className="flex gap-4 h-full px-3.5">
+          <button
+            onClick={handleLoginState}
+            className="min-w-9 grow px-4 my-auto h-8 border text-red-500 border-red-500 rounded-full"
+          >
+            Log in
+          </button>
+          <button
+            onClick={handleSignUpState}
+            className="min-w-9 grow px-4 my-auto h-8 text-white bg-red-500 rounded-full"
+          >
+            Sign up
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/MobileNav.js
+++ src/Components/MobileNav.js
@@ -1,17 +1,33 @@
 import React from "react";
 import img from "../pic.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagicWandSparkles } from "@fortawesome/free-solid-svg-icons";
-function MobileNav() {
+import {
+  faMagicWandSparkles,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
+function MobileNav(props) {
+  let { loggedIn } = props;
   return (
     <div className="px-4 w-full flex h-[53px] flex-row fixed top-0 right-0 left-0 bg-white/[99%]">
-      <div className="flex h-full justify-center shrink min-w-14">
-        <img src={img} className="rounded-full my-auto h-8" alt="yep" />
+      <div className="flex h-full justify-center shrink min-w-[56px]">
+        {/* Is logged in use img, else use logo */}
+        {loggedIn && (
+          <img src={img} className="rounded-full my-auto h-8" alt="yep" />
+        )}
+        {!loggedIn && (
+          <FontAwesomeIcon
+            icon={faMusic}
+            className="text-red-500 my-auto"
+            size="xl"
+          />
+        )}
       </div>
       <div className="my-auto grow max-w-[456px]">
         <h1 className="my-auto text-bold text-lg">Home</h1>
       </div>
       <div className="min-w-14 flex justify-center shrink items-end">
+        {/* this is supposed to be */}
+        {/* some sort of filtering, we will see. */}
         <FontAwesomeIcon
           icon={faMagicWandSparkles}
           className="h-5 w-5 flex my-auto"
